930bd7143658f768a949dfc2ed8b3f9824a995d8
FalloutBoy379 <anshmmehta379@gmail.com>
2024-04-15 22:09:30 -0400

    Tried to render cube using assimp. main is the binary file. The STL viewer was blank. Trying again by revisiting assimp basics.

---
+++ PrinterRenderer/Ubuntu_Attempt/file_load_test.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <assimp/Importer.hpp>
+#include <assimp/scene.h>
+#include <assimp/postprocess.h>
+
+// Function declarations
+
+
+int main() {
+    
+}
