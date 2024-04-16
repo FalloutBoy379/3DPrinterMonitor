ffc378a93e23228b82ce2542abfa02326622ecea
FalloutBoy379 <anshmmehta379@gmail.com>
2024-04-15 22:19:53 -0400

    Successfully read the meshes from the file. Next step is to render the meshes, which is proving to be a difficult task.

--- PrinterRenderer/Ubuntu_Attempt/file_load_test.cpp
+++ PrinterRenderer/Ubuntu_Attempt/file_load_test.cpp
@@ -1,14 +1,25 @@
-#include <iostream>
-#include <vector>
-#include <GL/glew.h>
-#include <GLFW/glfw3.h>
 #include <assimp/Importer.hpp>
 #include <assimp/scene.h>
 #include <assimp/postprocess.h>
-
-// Function declarations
-
+#include <iostream>
 
 int main() {
-    
+    Assimp::Importer importer;
+
+    const aiScene* scene = importer.ReadFile("/home/ansh/Desktop/EC535/3DPrinterMonitor/Cube_3d_printing_sample.stl", 
+        aiProcess_Triangulate | aiProcess_JoinIdenticalVertices | aiProcess_SortByPType);
+
+    if(!scene) {
+        std::cerr << "Error: " << importer.GetErrorString() << std::endl;
+        return 1;
+    }
+
+    std::cout << "Successfully imported STL File" << scene->mNumMeshes << " Meshes" << std::endl;
+
+    if (scene->mNumMeshes > 0) {
+        const aiMesh* mesh = scene->mMeshes[0];
+        std::cout << "Mesh has " << mesh->mNumVertices << " vertices." << std::endl;
+    }
+
+    return 0;
 }
